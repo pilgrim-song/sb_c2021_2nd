88bced7e575f0521ace3e4a1683854a0556e87ec
pilgrim-song <mailislove@gmail.com>
2021-10-11 00:41:21 +0900

    count 변수 초기화 하는 액션 추가

--- src/main/java/com/song/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/song/exam/demo/controller/UsrHomeController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	//private int count = 0; 여기서 초기화 하는 것보 생성자에서 초기화 하는 것이 좋다
+	private int count;
+	
+	public UsrHomeController() {
+		count = 0;
+	}
 	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
@@ -24,5 +30,19 @@ public class UsrHomeController {
 	public String showMain3() {
 		return "또 만나요!!!";
 	}
+	
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		return count++;
+	}
+	
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count 값이 0으로 초기화 되었습니다.";
+	}
+
 
 }
