90cc51d44f6ebd630ec55f9f8300b698c9c4f39d
pilgrim-song <mailislove@gmail.com>
2021-10-11 12:29:20 +0900

    getArticle, deleteArticle 서비스 메서드로 게시물 삭제

--- src/main/java/com/song/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/song/exam/demo/controller/UsrArticleController.java
@@ -11,9 +11,12 @@ import com.song.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
+	// 인스턴스 변수 시작
 	private int articlesLastId;
 	private List<Article> articles;
+	// 인스턴스 변수 끝
 	
+	// 생성자
 	public UsrArticleController() {
 		articlesLastId = 0;
 		articles = new ArrayList<>();
@@ -22,6 +25,7 @@ public class UsrArticleController {
 		makeTestData();
 	}
 
+	// 서비스 메서드 시작
 	private void makeTestData() {
 		for (int i = 1; i <=10; i++) {
 			String title = "제목" + i;
@@ -40,7 +44,24 @@ public class UsrArticleController {
 		
 		return article;
 	}
+	
+	private Article getArticle(int id) {
+		for (Article article : articles) {
+			if ( article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
+	private void deleteArticle(int id) {
+		Article  article = getArticle(id);
+		
+		articles.remove(article);
+	}
+	// 서비스 메서드 끝
 
+	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -49,9 +70,29 @@ public class UsrArticleController {
 		return article;
 	}
 	
+	// 액션 메서드 끝
+	
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articles;
 	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticle(id);
+		
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		deleteArticle(id);
+		
+		return id + "번 게시물을 삭제 하였습니다.";
+	}
+
+
+
+
 }
