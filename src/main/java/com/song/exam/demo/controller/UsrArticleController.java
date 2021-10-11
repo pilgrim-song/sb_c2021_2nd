729d59c5a606a469cec840bbfa679b809707eef3
pilgrim-song <mailislove@gmail.com>
2021-10-11 13:31:41 +0900

    게시물 수정

--- src/main/java/com/song/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/song/exam/demo/controller/UsrArticleController.java
@@ -70,6 +70,14 @@ public class UsrArticleController {
 		return article;
 	}
 	
+
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);	
+	}
+	
 	// 액션 메서드 끝
 	
 	@RequestMapping("/usr/article/getArticles")
@@ -91,6 +99,21 @@ public class UsrArticleController {
 		
 		return id + "번 게시물을 삭제 하였습니다.";
 	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doMidify(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		modifyArticle(id, title, body);
+		
+		return id + "번 게시물을 수정 하였습니다.";
+	}
+
 
 
 
