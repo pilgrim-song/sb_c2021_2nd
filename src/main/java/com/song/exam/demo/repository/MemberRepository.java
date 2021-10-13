1eb23188054dbaee1611abb819e03d81230458dd
pilgrim-song <mailislove@gmail.com>
2021-10-14 07:57:05 +0900

    MemberRepository 추가 후, 회원가입 처리

---
+++ src/main/java/com/song/exam/demo/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.song.exam.demo.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberRepository {
+
+	void join(@Param("loginId") String loginId, @Param("loginPw") String loginPw, @Param("name") String name, 
+			@Param("nickname") String nickname, @Param("cellphoneNo") String cellphoneNo, @Param("email") String email);
+
+}
