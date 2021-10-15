3ca6bca9ed73f467158f5331490a33e89d4ab8cf
pilgrim-song <mailislove@gmail.com>
2021-10-15 23:38:31 +0900

    /usr/article/doAdd 에 ResultData 적용

--- src/main/java/com/song/exam/demo/service/ArticleService.java
+++ src/main/java/com/song/exam/demo/service/ArticleService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.song.exam.demo.repository.ArticleRepository;
+import com.song.exam.demo.util.Ut;
 import com.song.exam.demo.vo.Article;
+import com.song.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -26,9 +28,11 @@ public class ArticleService {
 		}
 	}
 
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);	
-		return articleRepository.getLastInsertId();
+		int id =  articleRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
 	}
 
 	public List<Article> getArticles() {
