9479983102f1a5b19fab813adf277d7a7566e25c
pilgrim-song <mailislove@gmail.com>
2021-10-16 13:01:11 +0900

    세시물 테이블에 회운정보 추가

--- db/schema.sql
+++ db/schema.sql
@@ -99,3 +99,13 @@ cellphoneNo = '01012341234',
 email = 'mailislove@daum.net';
 
 select * from member;
+
+#게시물 테이블에 회원정보 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
+
+DESC article;
+
+SELECT * FROM article;
+
+# 기존 게시물의 작성자를 2번으로 지정 : MySQL 워크벤치 - Edit >> Preferences 메뉴 클릭 - SQL Editor 설정에서 제일 아래쪽 Other 설정 Safe Updates 체크 해제 - MySQL Workbench 재시작
+UPDATE article SET memberId = 2 WHERE memberId = 0;
